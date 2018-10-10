3f6fe7ba5c5553e440cadd4c962377efbcaf9380
reactuser02 <react.user.02@gmail.com>
2018-10-11 01:12:31 +0530

    Fixed the css color

--- src/components/elements/MovieInfoBar/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -5,26 +5,24 @@ import './MovieInfoBar.css';
 
 const MovieInfoBar = ({ time, budget, revenue }) => (
   <div className="rmdb-movieinfobar">
-    <div className="rmdb-movieinfo-content">
-      <div className="rmdb-rmdb-movieinfobar-content-col">
-        <FontAwesome className="fa-time" name="clock-o" size="2x" />
-        <span className="rmdb-movieinfobar-info">
-          Running time:
-          {calcTime(time)}
-        </span>
-      </div>
-      <div className="rmdb-rmdb-movieinfobar-content-col">
-        <FontAwesome className="fa-budget" name="money" size="2x" />
-        <span className="rmdb-movieinfobar-info">
-          Budget: {convertMoney(budget)}
-        </span>
-      </div>
-      <div className="rmdb-rmdb-movieinfobar-content-col">
-        <FontAwesome className="fa-revenue" name="ticket" size="2x" />
-        <span className="rmdb-movieinfobar-info">
-          Revenue: {convertMoney(revenue)}
-        </span>
-      </div>
+    <div className="rmdb-rmdb-movieinfobar-content-col">
+      <FontAwesome className="fa-time" name="clock-o" size="2x" />
+      <span className="rmdb-movieinfobar-info">
+        Running time:
+        {calcTime(time)}
+      </span>
+    </div>
+    <div className="rmdb-rmdb-movieinfobar-content-col">
+      <FontAwesome className="fa-budget" name="money" size="2x" />
+      <span className="rmdb-movieinfobar-info">
+        Budget: {convertMoney(budget)}
+      </span>
+    </div>
+    <div className="rmdb-rmdb-movieinfobar-content-col">
+      <FontAwesome className="fa-revenue" name="ticket" size="2x" />
+      <span className="rmdb-movieinfobar-info">
+        Revenue: {convertMoney(revenue)}
+      </span>
     </div>
   </div>
 );
